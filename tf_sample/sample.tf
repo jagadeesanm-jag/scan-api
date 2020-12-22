417a808374943d98e85174f7d9481fc166dea78e
Jagadeesan Murugesan <jagadeesan.m@gmail.com>
2020-12-22 19:51:26 +0530

    added the sample

--- tf_sample/sample.tf
+++ tf_sample/sample.tf
@@ -1,5 +1,19 @@
-terraform {  required_version = "0.11.8"}
+terraform {
+  required_version = "<=0.11.8"
+}
 
-provider "aws" {region = "us-east-1"}
+provider "aws" {
+region = "us-east-1"
+}
 
-resource "aws_s3_bucket" "foo" {bucket = "my-tf-log-bucket"acl = "private"versioning {enabled = true}logging {target_bucket = "logging_bucket"target_prefix = "log/"}}
+resource "aws_s3_bucket" "foo" {
+bucket = "my-tf-log-bucket"
+acl = "private"
+versioning {
+enabled = true
+}
+logging {
+target_bucket = "logging_bucket"
+target_prefix = "log/"
+}
+}
