89a26ab6b957398af1c21741888403f9370cdc2f
Jagadeesan Murugesan <jagadeesan.m@gmail.com>
2020-12-22 19:42:23 +0530

    added the configuration

--- tf_sample/sample.tf
+++ tf_sample/sample.tf
@@ -1,19 +1,5 @@
-terraform {
-  required_version = ">=0.1000000000.0"
-}
+terraform {  required_version = "0.11.8"}
 
-provider "aws" {
-region = "us-east-1"
-}
+provider "aws" {region = "us-east-1"}
 
-resource "aws_s3_bucket" "foo" {
-bucket = "my-tf-log-bucket"
-acl = "private"
-versioning {
-enabled = true
-}
-logging {
-target_bucket = "logging_bucket"
-target_prefix = "log/"
-}
-}
+resource "aws_s3_bucket" "foo" {bucket = "my-tf-log-bucket"acl = "private"versioning {enabled = true}logging {target_bucket = "logging_bucket"target_prefix = "log/"}}
